ce8dee023c5c77c095b21d05186ce0aca9e06edb
schochastics <david@schochastics.net>
2024-02-18 20:42:04 +0100

    bumped version of nutshell and removed shell next to header #4

--- _extensions/nutshell/nutshell.lua
+++ _extensions/nutshell/nutshell.lua
@@ -1,7 +1,7 @@
 function Pandoc(doc)
-  quarto.doc.addHtmlDependency({
-    name = "nutshell",
-    version = "1.0.0",
-    scripts = {"nutshell.min.js"}
-  })
+    quarto.doc.addHtmlDependency({
+        name = "nutshell",
+        version = "1.0.7",
+        scripts = { "nutshell.js", "nutshell_options.js" }
+    })
 end
---
+++ _extensions/nutshell/nutshell_options.js
@@ -0,0 +1,5 @@
+Nutshell.setOptions({
+    startOnLoad: true, // Start Nutshell on load? (default: true)
+    lang: 'en', // Language (default: 'en', which is English)
+    dontEmbedHeadings: true, // If 'true', removes the "embed this as a nutshell" option on headings
+});
